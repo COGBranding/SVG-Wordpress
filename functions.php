ba85798c7e825287dc4fa913fba2ed067ada8d9b
COG Branding <81595974+COGBranding@users.noreply.github.com>
2022-02-17 11:49:03 +1100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+//This function can be used to add multiple file types to be accepted by Wordpress core.
+
+function add_file_types_to_uploads($file_types){
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+    return $file_types;
+}
+add_filter('upload_mimes', 'add_file_types_to_uploads');
